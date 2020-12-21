1f6a68977c129480ed44d06c4f619f8537e27991
e-Watson <eduard.skripchenko@icloud.com>
2020-12-21 21:24:55 +0200

    2nd and 3rd tasks

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const destArr = [...arr];
 
+  const ascSortFn = (a, b) => a.localeCompare(b, ['ru', 'en'], { caseFirst: 'upper' });
+  const descSortFn = (a, b) => b.localeCompare(a, ['ru', 'en'], { caseFirst: 'upper' });
+
+  return param === 'asc' ? destArr.sort(ascSortFn) : destArr.sort(descSortFn);
 }
