dc5387e40240d1320d6fd8746fa8549a9d616b3a
e-Watson <eduard.skripchenko@icloud.com>
2021-01-14 09:05:15 +0200

    5th task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,46 @@
 export default class NotificationMessage {
+  static notification = null;
 
+  render() {
+    const element = document.createElement("div");
+    element.innerHTML = this.getHtmlTemplate();
+    this.element = element.firstElementChild;
+    NotificationMessage.notification = this.element;
+  }
+
+  show(target = document.body) {
+    target.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.element = null;
+    NotificationMessage.notification = null;
+  }
+
+  constructor(text = '', { type = '', duration = 1000 } = {}) {
+    this.text = text;
+    this.type = type;
+    this.duration = duration;
+
+    this.render();
+  }
+
+  getHtmlTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${(this.duration / 1000).toFixed(0)}s">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+              <div class="notification-header">${this.type}</div>
+              <div class="notification-body">
+                  ${this.text}
+              </div>
+          </div>
+      </div>
+    `;
+  }
 }
